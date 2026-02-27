962a31c651a96ffcff3b5e331027b02f077f6756
kaberly <257349530+kaberly@users.noreply.github.com>
2026-02-27 17:01:22 +0100

    Send index to callcack in registerDomNodeMutatedUnique

--- snippet/registerDomNodeMutatedUnique.js
+++ snippet/registerDomNodeMutatedUnique.js
@@ -7,20 +7,22 @@
  * Ensure that when an element matching the query elementProvider, the callback is called with the element 
  * exactly once for each element
  * @param {()=>[HTMLElement]} elementProvider 
- * @param {(element: HTMLElement)=>{}} callback 
+ * @param {(element: HTMLElement, index: number)=>{}} callback 
  */
 const registerDomNodeMutatedUnique = (elementProvider, callback) => {
     const domNodesHandled = new Set()
 
     return registerDomNodeMutated(() => {
+        let index = 0
         for (let element of elementProvider()) {
             if (!domNodesHandled.has(element)) {
                 domNodesHandled.add(element)
-                const result = callback(element)
+                const result = callback(element, index)
                 if (result === false) {
                     domNodesHandled.delete(element)
                 }
             }
+            index++;
         }
     })
 }
