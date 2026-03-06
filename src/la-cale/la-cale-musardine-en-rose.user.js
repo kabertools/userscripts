8f9020f295304fb93a35ec47c4e5a2a4e8a022f5
kaberly <257349530+kaberly@users.noreply.github.com>
2026-03-06 20:46:52 +0100

    CLEANUP

--- src/la-cale/la-cale-musardine-en-rose.user.js
+++ src/la-cale/la-cale-musardine-en-rose.user.js
@@ -6,22 +6,25 @@
 const main = async () => {
     registerLocationChange((location) => {
         if (location.pathname === '/taverne') {
-            registerDomNodeMutatedUnique(() => getElements('.flex-1.overflow-hidden'), (node) => {
-                const userMessages = getSubElements(node, '[href="/profile/Musardine"]')
-                if (userMessages.length > 0) {
-                    console.log({ userMessages })
-                    const nameElement = getSubElements(userMessages[0], 'span')[0]
-                    if (nameElement) {
-                        nameElement.style.color = 'rgb(223,42,132)'
-                        const nextSibling = userMessages[0]?.nextElementSibling?.nextElementSibling
-                        if (nextSibling) {
-                            nextSibling.style.color = 'rgb(223,42,132)'
-                            nextSibling.style.borderColor = 'rgb(223,42,132,0.45)'
-                            nextSibling.style.backgroundColor = 'rgb(223,42,132,0.12)'
+            registerDomNodeMutatedUnique(
+                () => getElements('.flex-1.overflow-hidden'),
+                (node) => {
+                    const userMessages = getSubElements(node, '[href="/profile/Musardine"]')
+                    if (userMessages.length > 0) {
+                        console.log({ userMessages })
+                        const nameElement = getSubElements(userMessages[0], 'span')[0]
+                        if (nameElement) {
+                            nameElement.style.color = 'rgb(223,42,132)'
+                            const nextSibling = userMessages[0]?.nextElementSibling?.nextElementSibling
+                            if (nextSibling) {
+                                nextSibling.style.color = 'rgb(223,42,132)'
+                                nextSibling.style.borderColor = 'rgb(223,42,132,0.45)'
+                                nextSibling.style.backgroundColor = 'rgb(223,42,132,0.12)'
+                            }
                         }
                     }
                 }
-            })
+            )
         }
     })
 }
